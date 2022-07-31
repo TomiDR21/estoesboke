789620009c942a34525f65268a5bd53a12a804d4
tomi <tomidirisio@gmail.com>
2022-07-31 15:03:29 -0300

    estoesboke 0.0.1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import Home from "./components/Home";
+import "./index.css";
+import TagButton from "./components/TagButton";
+import images from "./components/imagenes";
+import { Routes, Route, Link } from "react-router-dom";
+import MemesContainer from "./components/MemesContainer";
+
+
+
+
+function App() {
+
+
+
+
+  return (
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+        <Route path="filtrador" element={<MemesContainer />} />
+      </Routes>
+    
+  );
+}
+
+
+
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import MemesContainer from './MemesContainer';
+
+
+
+
+const Home = () => 
+    <div>
+    <MemesContainer></MemesContainer>
+    
+
+    <div className='container'>
+        <div className='texto-boke'>
+            Buscá tu meme BOKE con los FILTROS de arriba <br></br>
+            
+           
+
+        </div>   
+    </div>
+
+    </div>
+
+    
+export default Home
